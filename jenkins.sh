ce6e719138c266c40dd8c4f2153b588008604b45
rajuchennuri2308 <rajuchennuri2308@gmail.com>
2024-12-19 21:55:16 +0100

    changes to main branch

--- jenkins.sh
+++ jenkins.sh
@@ -10,7 +10,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #STEP-3: DOWNLOAD JAVA11
-sudo yum clean all
+#sudo yum clean all
 sudo yum update -y
 #sudo amazon-linux-extras enable java-openjdk11
 #sudo yum install -y java-11-openjdk
