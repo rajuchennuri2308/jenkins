1a1549ccd22aeca133c9b1dce4588e6c67a85626
rajuchennuri2308 <rajuchennuri2308@gmail.com>
2024-12-20 20:52:29 +0100

    Update jenkins.sh
    Signed-off-by: rajuchennuri2308 <rajuchennuri2308@gmail.com>

--- jenkins.sh
+++ jenkins.sh
@@ -1,31 +1,27 @@
-#STEP-1: INSTALLING GIT 
-
-ECH0 ##########INSTALLING GIT ########
+#STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
 yum install git -y
-
-#STEP-2: INSTALLING MAVEN
-
-ECHO ##########INSTALLING MAVEN ###########
+#java-1.8.0-openjdk 
 yum install maven -y
 
-#step-4: INSTALLING java
-
-ECHO #########INSTALLING JAVA#######
-
-sudo yum install fontconfig java-17-openjdk-y
-
-#STEP-4: GETTING THE REPO (jenkins.io --> download -- > redhat)
+#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-#STEP-5 : Installing jenkins
-yum install jenkins -y
-update-alternatives --config java
-sudo systemctl daemon-reload
+#STEP-3: DOWNLOAD JAVA11
+sudo yum clean all
+sudo yum update -y
+sudo rpm --import https://yum.corretto.aws/corretto.key
+sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+sudo yum install fontconfig java-17-openjdk -y
 
-#STEP-6: RESTARTING JENKINS (when we download service it will on stopped state)
+#STEP-4 : Installing jenkins
+yum install jenkins -y
+
+
+#STEP-5: RESTARTING JENKINS (when we download service it will on stopped state)
+systemctl daemon-reload
 systemctl start jenkins.service
+sudo usermod -a -G docker jenkins
 systemctl status jenkins.service
 
 exit
-## end of the script##
